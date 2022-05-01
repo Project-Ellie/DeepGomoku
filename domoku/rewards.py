c99e52382a80f66ab3c48b0bdce5cf8ad7bf66cb
Wolfgang <wolfgang.giersche@adtrac.ch>
2022-05-01 16:07:59 +0200

    Genetic algorith first experiences

--- domoku/rewards.py
+++ domoku/rewards.py
@@ -1,6 +1,8 @@
 import numpy as np
 import tensorflow as tf
 
+from domoku.data import create_binary_rep, create_binary_action
+
 
 class InfluenceReward(tf.keras.Model):
     """
@@ -8,14 +10,17 @@ class InfluenceReward(tf.keras.Model):
     of the own stones vs the oppenent's stones' influence by that move
     """
 
-    def __init__(self, board_size, current_influence, other_influence=None, tau=2.0, **kwargs):
+
+    def __init__(self, board_size, curr_tau, other_tau, current_influence, other_influence=None, **kwargs):
         """
 
-        :param current_influence: The array of the current player's influence values from remote to close
+        :param current_influence: The array of the current player's influence values from remote to close,
+            first array of 4 is for vertical/horizontal, the second for diagonal directions
         :param other_influence: The array of the other player's influence values from remote to close,
             defaults to current_influence
         :param board_size: The size of the original board
-        :param tau: The geometric coefficient to compute combined influence, defaults to 2.0
+        :param curr_tau: The geometric coefficient to compute combined influence, example: 2.0 for cartesion
+        :param other_tau: The geometric coefficient to compute combined influence, example: 2.0 for cartesion
         :param kwargs: anything that needs to go to keras.Model
         """
         other_influence = other_influence if other_influence is not None else current_influence
@@ -27,14 +32,15 @@ class InfluenceReward(tf.keras.Model):
         # sufficient padding for the given range
         self.input_size = 2 * self.range_i + board_size
         # geometric coefficient
-        self.tau = tau
+        self.curr_tau = curr_tau
+        self.other_tau = other_tau
         self.max_reward = 10
 
-        assert len(current_influence) == len(other_influence) == self.range_i, \
+        assert len(current_influence[0]) == len(other_influence[0]) == self.range_i, \
             f"Influence rays must have length {self.range_i}"
 
         self.filters = self.create_influence_filters(current_influence, other_influence)
-        print(self.filters.shape)
+
         kernel_init = tf.constant_initializer(self.filters)
 
         self.input_layer = tf.keras.layers.Conv2D(filters=4, kernel_size=(2 * self.range_i + 1, 2 * self.range_i + 1),
@@ -49,6 +55,7 @@ class InfluenceReward(tf.keras.Model):
 
         self.terminated = self.create_termination_detector()
 
+
     def create_termination_detector(self):
         five = [0, 0, 1, 1, 1, 1, 1, 0, 0]
         empty = np.zeros([9, 9], dtype=float)
@@ -62,11 +69,6 @@ class InfluenceReward(tf.keras.Model):
             np.array([np.diag(five)[::-1], empty]),
             np.array([horiz, empty]),
             np.array([vert, empty]),
-
-            # np.array([empty, np.diag(five)]),
-            # np.array([empty, np.diag(five)[::-1]]),
-            # np.array([empty, horiz]),
-            # np.array([empty, vert])
         ])
         filters = np.rollaxis(np.rollaxis(filters, 1, 4), 0, 4)
         kernel_init = tf.constant_initializer(filters)
@@ -79,68 +81,125 @@ class InfluenceReward(tf.keras.Model):
 
     def reshape(self, s, a):
         s = np.reshape(s, [-1, self.input_size, self.input_size, 2])
-        a = np.reshape(a, [-1, self.input_size, self.input_size, 2])
+        if a is not None:
+            a = np.reshape(a, [-1, self.input_size, self.input_size, 2])
         return s, a
 
 
-    def call(self, s, a):
+    def call(self, s, a, switch=False):
         s, a = self.reshape(s, a)
         current_player_after = self.input_layer(s + a)
         current_player_before = self.input_layer(s)
-        combined = self.combine(current_player_after) - self.combine(current_player_before)
+        tau = self.other_tau if switch else self.curr_tau
+        combined = (self.combine(current_player_after, tau) -
+                    self.combine(current_player_before, tau))
 
         if self.is_terminated(s):
-            return -self.max_reward
+            return -self.max_reward, True
         if self.is_terminated(s + a):
-            return self.max_reward
+            return self.max_reward, True
         else:
-            return tf.squeeze(tf.reduce_sum(combined)).numpy()
+            return tf.squeeze(tf.reduce_sum(combined)).numpy(), False
+
 
     def is_terminated(self, state):
         s = np.reshape(state, [-1, self.input_size, self.input_size, 2])
         return tf.reduce_sum(self.terminated(s)) >= 1
 
-    def combine(self, influences):
-        return self.combiner(influences ** self.tau)  # ** (1/self.tau)
+
+    def combine(self, influences, tau):
+        return self.combiner(influences ** tau)  # ** (1/self.tau)
 
 
-    def influence_of(self, s, a):
+    def influence_of(self, s, a=None, for_humans=True):
         """
         Utility method to display the direct influence from the current player's point of view
         """
         s, a = self.reshape(s, a)
-        current_player_after = self.input_layer(s + a)
-        combined = self.combine(current_player_after)
+        state = s + a if a is not None else s
+        current_player_after = self.input_layer(state)
+        combined = self.combine(current_player_after, self.curr_tau)
         orig = tf.squeeze(combined)
-        i_max = tf.reduce_max(orig)
-        return tf.floor(orig / i_max * 9.99)
+        if for_humans:
+            i_max = tf.reduce_max(orig)
+            return tf.floor(orig / i_max * 9.99)
+        else:
+            return orig
 
 
     def create_influence_filters(self, current_influence, other_influence):
         """
         Create filters for each general direction
         :param current_influence: array representing the influence of current player in any direction,
-            like e.g. [1, 2, 4, 8]
+            like e.g. [[1, 2, 4, 8], [1, 2, 4, 8]] for h/v and diag
         :param other_influence: array representing the influence of the other player in any direction,
-            like e.g. [1, 2, 4, 8]
+            like e.g. [[1, 2, 4, 8], [1, 2, 4, 8]] for h/v and diag
         :return: an np array representing that influence in a CNN
         """
         kernel_size = 2 * self.range_i + 1
 
         # Step 1: combine opposite directions and suppress influence on occupied fields
-        supp_c = -sum(current_influence) * 8
-        current = current_influence + [supp_c] + current_influence[::-1]
+        supp_c = -np.sum(np.array(current_influence)) * 8
+        current_hv = current_influence[0] + [supp_c] + current_influence[0][::-1]
+        current_diag = current_influence[1] + [supp_c] + current_influence[1][::-1]
 
-        supp_o = sum(other_influence) * 8
-        other = other_influence + [supp_o] + other_influence[::-1]
-        other = - np.array(other)
+        supp_o = np.sum(np.array(other_influence)) * 8
+        other_hv = -(np.array(other_influence[0] + [supp_o] + other_influence[0][::-1]))
+        other_diag = -(np.array(other_influence[1] + [supp_o] + other_influence[1][::-1]))
 
-        ne = np.array([np.diag(current)[::-1], np.diag(other)[::-1]])
-        nw = np.array([np.diag(current), np.diag(other)])
+        ne = np.array([np.diag(current_diag)[::-1], np.diag(other_diag)[::-1]])
+        nw = np.array([np.diag(current_diag), np.diag(other_diag)])
         w = np.zeros([2, kernel_size, kernel_size])
-        w[0, self.range_i, :] = current
-        w[1, self.range_i, :] = other
+        w[0, self.range_i, :] = current_hv
+        w[1, self.range_i, :] = other_hv
         n = np.zeros([2, kernel_size, kernel_size])
-        n[0, :, self.range_i] = current
-        n[1, :, self.range_i] = other
+        n[0, :, self.range_i] = current_hv
+        n[1, :, self.range_i] = other_hv
         return np.rollaxis(np.rollaxis(np.array([n, w, ne, nw]), 1, 4), 0, 4).astype(float)
+
+
+class RewardContext:
+    """
+    Just a tool to examine the usefulness of reward models
+    """
+
+
+    def __init__(self, the_board, reward_model, offensiveness=0.5):
+        self.board = the_board
+        self.model = reward_model
+        self.selector = InfluenceReward(board_size=self.board.N, curr_tau=2, other_tau=2,
+                                        current_influence=[[1, 2, 4, 8], [1, 2, 4, 8]])
+        self.offensiveness = offensiveness
+        self.cut_off_offensive = 2
+        self.cut_off_defensive = 2
+
+
+    def reward(self, move):
+        # current player's point of view
+        state = create_binary_rep(self.board, padding=4, border=True)
+        action = create_binary_action(self.board.N, 4, move)
+        offensive_reward, terminated = self.model(state, action)
+
+        # adversary's point of view
+        state_adv = create_binary_rep(self.board, padding=4, border=True, switch=True)
+        action_adv = create_binary_action(self.board.N, 4, move, switch=True)
+        defensive_reward, terminated_adv = self.model(state_adv, action_adv)
+
+        o = self.offensiveness
+        return 2 * o * offensive_reward - 2 * (1 - o) * defensive_reward, terminated or terminated_adv
+
+
+    def find_candidates(self):
+        n = self.board.N
+
+        state = create_binary_rep(self.board, padding=4, border=True)
+        me = self.selector.influence_of(state, None)
+        offensive = {(me[i, j].numpy(), i, j) for i in range(n) for j in range(n)
+                     if me[i, j] > self.cut_off_offensive}
+
+        state_adv = create_binary_rep(self.board, padding=4, border=True, switch=True)
+        other = self.selector.influence_of(state_adv, None)
+        defensive = {(other[i, j].numpy(), i, j) for i in range(n) for j in range(n)
+                     if other[i, j] > self.cut_off_defensive}
+
+        return offensive.union(defensive)
