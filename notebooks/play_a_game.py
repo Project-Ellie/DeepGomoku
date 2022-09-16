22a95a6569e44f7c782b2e5f128892979dc70286
wgiersche <wgiersche@gmail.com>
2022-09-16 14:09:29 +0200

    successful arena 3, but opening advantage too dominant

--- notebooks/play_a_game.py
+++ notebooks/play_a_game.py
@@ -7,6 +7,6 @@ config = CompManConfig(board_size=19)
 if __name__ == '__main__':
     #  initializer = RandomBoardInitializer(config.board_size, 4, 6, 8, 6, 8)
     initializer = ConstantBoardInitializer("")
-    ai = "1_c3.model"
-    num_simulations = 1200
+    ai = "3_c2.model"
+    num_simulations = 600
     StudyHandler(config, "../cmclient/gui/", initializer, ai, num_simu=num_simulations).handle()
